fddcfe5c87d0c8bff530b3ec95c8007da4e3e7a5
Tobias Ninz <tobias.ninz@demaclenko.com>
2022-05-25 12:55:07 +0200

    Added icon to title bar, made background settable

--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -2,6 +2,8 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
+#include "Walnut/Random.h"
+#include "Walnut/Timer.h"
 
 using namespace Walnut;
 
@@ -11,12 +13,15 @@ public:
 	virtual void OnUIRender() override
 	{
 		ImGui::Begin("Settings");
+		ImGui::DockBuilderDockWindow("Settings", ImGui::GetActiveID());
+		ImGui::Text("Last render: %.3fms", m_LastRenderTime);
 		if (ImGui::Button("Render")) 
 		{
 			Render();
 		}
 		ImGui::End();
 
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
 		ImGui::Begin("Viewport");
 
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
@@ -25,11 +30,15 @@ public:
 		if(m_Image)
 			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth() , (float)m_Image->GetHeight() });
 
-		ImGui::ShowDemoWindow();
+		ImGui::End();
+		ImGui::PopStyleVar();
+		Render();
 	}
 
 	void Render()
 	{
+		Timer timer;
+
 		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeigth != m_Image->GetHeight())
 		{
 			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeigth, ImageFormat::RGBA);
@@ -37,28 +46,36 @@ public:
 			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeigth];
 		}
 
-		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeigth; i++) 
+		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeigth; i++)
 		{
-			m_ImageData[i] = 0xffff00ff;
+			m_ImageData[i] = Random::UInt();
+			m_ImageData[i] != 0xffff00ff;
 		}
 
 		m_Image->SetData(m_ImageData);
+
+		m_LastRenderTime = timer.ElapsedMillis();
 	}
 private:
 	std::shared_ptr<Image> m_Image;
 	uint32_t m_ViewportWidth = 0;
 	uint32_t m_ViewportHeigth = 0;
 	uint32_t* m_ImageData = nullptr;
+
+	float m_LastRenderTime = 0.0f;
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 {
 	Walnut::ApplicationSpecification spec;
 	spec.Name = "Ray Tracing";
+	spec.ChildBg = { 44, 43, 56, 255 };
+	spec.ParentBg = { 44, 43, 65, 255 };
+	spec.icon = "C:\\Users\\i0006683\\Documents\\Projects\\RayTracing\\bin\\Debug-windows-x86_64\\RayTracing\\icon.png";
 
 	Walnut::Application* app = new Walnut::Application(spec);
 	app->PushLayer<ExampleLayer>();
-	app->SetMenubarCallback([app]()
+	/*app->SetMenubarCallback([app]()
 	{
 		if (ImGui::BeginMenu("File"))
 		{
@@ -68,6 +85,6 @@ Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 			}
 			ImGui::EndMenu();
 		}
-	});
+	});*/
 	return app;
 }
